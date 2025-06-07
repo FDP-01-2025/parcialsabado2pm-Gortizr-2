afe58bdf48b78c88a7eef683c98c83ff6b18b3f0
Gortizr <00131725@uca.edu.sv>
2025-06-07 15:48:50 -0600

    parcial 2

---
+++ Ejercicio3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    char user, carnet;
+    int op, libro[5];
+
+    cout << "Ingrese su numero nombre de usuario: \n";
+    cin >>user;
+    cout << "Ingrese su numero de carnet:\n";
+    cin >> carnet;
+
+    cout << "1.Prestar libro.\n";
+    cout << "2.Devolver libro.\n";
+    cout << "3.Salir.\n";
+    cin >> op;
+
+    switch(op){
+        case 1:
+        is_arra
+    }
+
+
+
+
+
+
+    return 0;
+}
